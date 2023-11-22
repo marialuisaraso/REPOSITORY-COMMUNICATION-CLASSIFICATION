81d7266d6138ba23b2c26ec0a53bd14b853c7ae1
Maria Luisa Raso <marialuisarasoo@gmail.com>
2023-11-22 00:13:33 -0300

    paginação v2 - todas separadas

---
+++ Paging/PagingIssues.py
@@ -0,0 +1,40 @@
+import requests
+
+url = 'https://api.github.com/graphql'
+headers = {'Authorization': 'Bearer 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'}
+
+def get_issues(cursor=None):
+    query = """
+    query {
+      repository(owner: "microsoft", name: "vscode") {
+        issues(first: 10, after: %s) {
+          nodes {
+            title
+            body
+          }
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+        }
+      }
+    }
+    """ % (f'"{cursor}"' if cursor else "null")
+
+    response = requests.post(url, json={'query': query}, headers=headers)
+    return response.json()
+
+# Inicialmente, obtemos as primeiras 10 issues sem cursor
+result = get_issues()
+
+with open('Paging/outputs/issuesOutput.txt', 'w') as file:
+    # Escrevemos os resultados no arquivo
+    file.write(str(result))
+
+# Iteramos para obter as próximas páginas usando os cursores
+while result['data']['repository']['issues']['pageInfo']['hasNextPage']:
+    cursor = result['data']['repository']['issues']['pageInfo']['endCursor']
+    result = get_issues(cursor)
+
+    with open('Paging/outputs/issuesOutput.txt', 'a') as file:
+        file.write(str(result))
--- Paging/PagingPrs.py
+++ Paging/PagingPrs.py
@@ -3,11 +3,11 @@ import requests
 url = 'https://api.github.com/graphql'
 headers = {'Authorization': 'Bearer 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'}
 
-def get_issues(cursor=None):
+def get_pull_requests(cursor=None):
     query = """
     query {
       repository(owner: "microsoft", name: "vscode") {
-        issues(first: 10, after: %s) {
+        pullRequests(first: 10, after: %s) {
           nodes {
             title
             body
@@ -24,17 +24,17 @@ def get_issues(cursor=None):
     response = requests.post(url, json={'query': query}, headers=headers)
     return response.json()
 
-# Inicialmente, obtemos as primeiras 10 issues sem cursor
-result = get_issues()
+# Inicialmente, obtemos as primeiras 10 pull requests sem cursor
+result = get_pull_requests()
 
-with open('output.txt', 'w') as file:
+with open('Paging/outputs/prsOutput.txt', 'w') as file:
     # Escrevemos os resultados no arquivo
     file.write(str(result))
 
 # Iteramos para obter as próximas páginas usando os cursores
-while result['data']['repository']['issues']['pageInfo']['hasNextPage']:
-    cursor = result['data']['repository']['issues']['pageInfo']['endCursor']
-    result = get_issues(cursor)
+while result['data']['repository']['pullRequests']['pageInfo']['hasNextPage']:
+    cursor = result['data']['repository']['pullRequests']['pageInfo']['endCursor']
+    result = get_pull_requests(cursor)
 
-    with open('issuesOutput.txt', 'a') as file:
+    with open('Paging/outputs/prsOutput.txt', 'a') as file:
         file.write(str(result))
