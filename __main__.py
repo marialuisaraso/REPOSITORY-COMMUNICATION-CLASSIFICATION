481827805b0e93f3096de33e62605fd2d340d6b6
Maria Luisa Raso <marialuisarasoo@gmail.com>
2024-03-03 15:59:10 -0300

    limpeza-main
    limpeza-main

--- __main__.py
+++ __main__.py
@@ -1,16 +1,11 @@
 from Vader.Vader.vaderSentiment import Vader
 
 def main():
-    print("MENU DE FERRAMENTAS ANÁLISE DE SENTIMENTOS PARA OS DADOS EXTRAÍDOS:")
-    print("1- VADER")
-    print("2- SENTISTRENGTH")
-
-    escolha = input("Digite o número da opção: ")
+    print("ANÁLISE DE SENTIMENTOS EM REPÓSITÓRIO MINERADO DO GITHUB")
+    escolha = input("DIGITE 1 PARA COMEÇAR: ")
 
     if escolha == "1":
         Vader()
-    if escolha == "2":
-        print("Opção 2")
 
 main()
 
