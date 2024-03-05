c528aa5fc91fecbde63426305c74279573aa7bcd
Maria Luisa Raso <marialuisarasoo@gmail.com>
2024-03-05 13:23:45 -0300

    LIMPANDO-CODIGO

--- SentiStrength/main.py
+++
@@ -1,8 +0,0 @@
-from sentistrength import PySentiStr
-
-senti = PySentiStr()
-senti.setSentiStrengthPath('/Users/marialuisatomichraso/Desktop/TCC-PROJETO/SentiStrength/SentiStrength_Data') 
-senti.setSentiStrengthLanguageFolderPath('/Users/marialuisatomichraso/Desktop/TCC-PROJETO/SentiStrength/SentStrength_Data')
-result = senti.getSentiment('hi')
-print(result)
-
