45cbab8d1e08274c634bdf135573d2b64b488a4f
Maria Luisa Raso <marialuisarasoo@gmail.com>
2024-03-05 13:19:26 -0300

    PAGING-PRS-AJUSTADO

--- Tools/Paging/PagingPrs.py
+++ Tools/Paging/PagingPrs.py
@@ -1,10 +1,12 @@
-import requests, os
+import requests
+import os
 
 def main():
-  url = 'https://api.github.com/graphql'
-  headers = {'Authorization': 'Bearer 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'}
+    # Lista de tokens de autenticação
+    tokens = ['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', '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', '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', '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']  # Adicione quantos tokens desejar
+    url = 'https://api.github.com/graphql'
 
-  def get_pull_requests(cursor=None):
+    def get_pull_requests(cursor=None, token=None):
       query = """
       query {
         repository(owner: "kamranahmedse", name: "developer-roadmap") {
@@ -22,43 +24,57 @@ def main():
       }
       """ % (f'"{cursor}"' if cursor else "null")
 
+      # Alternar entre os tokens de autenticação
+      headers = {'Authorization': f'Bearer {token}'}
       response = requests.post(url, json={'query': query}, headers=headers)
-      return response.json()
+      if response.status_code == 200:
+          return response.json()
+      # Se nenhum token funcionar, retornar None
+      return None
 
-  # Inicialmente, obtemos as primeiras 10 pull requests sem cursor
-  result = get_pull_requests()
+    # Inicialmente, obtemos os primeiros 10 commits sem cursor
+    result = get_pull_requests(None, tokens[0])
 
-  with open('Tools/Paging/outputs/prsOutput.txt', 'w') as file:
-      # Escrevemos os resultados no arquivo
-      file.write(str(result))
+    if result is not None:
+        tmp_json = []
+        with open('Tools/Paging/outputs/prsOutput.txt', 'w') as file:
+            # Escrevemos os resultados no arquivo
+            file.write(str(result))
 
-  # Iteramos para obter as próximas páginas usando os cursores
-  max_iter = 50
-  iter = 0
-  tmp_json = []
-  while result['data']['repository']['pullRequests']['pageInfo']['hasNextPage'] and iter < max_iter:
-      print("pagingPrs [{}/{}]".format(iter, max_iter))
+        # Iteramos para obter as próximas páginas usando os cursores
+        max_iter = 1200
+        iter = 0
+        while result['data']['repository']['pullRequests']['pageInfo']['hasNextPage'] and iter < max_iter:
+            print("pagingCommits [{}/{}]".format(iter, max_iter))
+            iter += 1
+            cursor = result['data']['repository']['pullRequests']['pageInfo']['endCursor']
+            
+            random_token = tokens[iter % len(tokens)]
 
-      iter += 1
-      cursor = result['data']['repository']['pullRequests']['pageInfo']['endCursor']
-      result = get_pull_requests(cursor)
-      nodes = result['data']['repository']['pullRequests']['nodes']
-      for node in nodes:
-        tmp_json.append(node)
-  print("end of while")
-  with open('Tools/Paging/outputs/prsOutput.txt', 'w') as file:
-      file.write(str(tmp_json))
+            result = get_pull_requests(cursor, random_token)
+            if result is not None:
+                nodes = result['data']['repository']['pullRequests']['nodes']
+                for node in nodes:
+                    tmp_json.append(node)
+            else:
+                break
 
-  with open('Tools/Paging/output_final/prsOutput.txt', 'w') as file:
-     pass
+        with open('Tools/Paging/outputs/prsOutput.txt', 'w') as file:
+            file.write(str(tmp_json))
 
-  for tmp in tmp_json:
-    with open('Tools/Paging/output_final/prsOutput.txt', 'a') as file:
-      tmp_text = str(tmp['body'])
-      tmp_text = tmp_text.replace('\n', '')
-      tmp_text = os.linesep.join([s for s in tmp_text.splitlines() if s])
-      tmp_text = tmp_text.replace('\n', '')
-      file.write(str(tmp_text) + '\n')
+        with open('Tools/Paging/output_final/prsOutput.txt', 'w') as file:
+            pass
+
+        for tmp in tmp_json:
+          with open('Tools/Paging/output_final/prsOutput.txt', 'a') as file:
+            tmp_text = str(tmp['body'])
+            tmp_text = tmp_text.replace('\n', '')
+            tmp_text = os.linesep.join([s for s in tmp_text.splitlines() if s])
+            tmp_text = tmp_text.replace('\n', '')
+            file.write(str(tmp_text) + '\n')
+    else:
+        print("Erro de autenticação.")
 
 
-# main()
+if __name__ == "__main__":
+    main()
