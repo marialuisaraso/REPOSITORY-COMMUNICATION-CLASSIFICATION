0040cf712b53343f255fa62733dca16e4acd417b
Maria Luisa Raso <marialuisarasoo@gmail.com>
2024-04-02 14:34:41 -0300

    limpeza do workspace

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from Vader.Vader.vaderSentiment import Vader
 
 def main():
     print("ANÁLISE DE SENTIMENTOS EM REPÓSITÓRIO MINERADO DO GITHUB")
-    escolha = input("DIGITE 1 PARA COMEÇAR: ")
+    escolha = input("DIGITE 1 PARA DISPARAR A ANÁLISE DE SENTIMENTOS: ")
 
     if escolha == "1":
         Vader()
