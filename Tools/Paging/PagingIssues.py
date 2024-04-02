f018924dbd61afd168c53cf5f65166f80a1be987
Maria Luisa Raso <marialuisarasoo@gmail.com>
2024-04-02 14:08:32 -0300

    ajuste na paginação de issues

--- Tools/Paging/PagingIssues.py
+++ Tools/Paging/PagingIssues.py
@@ -15,7 +15,6 @@ def main():
                 title
                 body
                 comments(first: 100) {
-                  totalCount
                   nodes {
                     author {
                       login
@@ -82,8 +81,8 @@ def main():
                 tmp_text = os.linesep.join([s for s in tmp_text.splitlines() if s])
                 tmp_text = tmp_text.replace('\n', '')
                 file.write(str(tmp_text) + '\n')
-            with open('Tools/Graphics/issuesComments.txt', 'a') as file2:
-                file2.write("Total de Comentários: " + str(tmp['comments']['totalCount']) + '\n')  # Adiciona a contagem de comentários
+            # with open('Tools/Graphics/issuesComments.txt', 'a') as file2:
+            #     file2.write("Total de Comentários: " + str(tmp['comments']['totalCount']) + '\n')  # Adiciona a contagem de comentários
     else:
         print("Erro de autenticação.")
 
